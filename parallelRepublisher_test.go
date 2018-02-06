bede231e7076ebc0045a4e6a8d770303e3fbc5ad
Hunor Kovacs <hunor.kovacs@ft.com>
2018-02-06 19:18:13 +0200

    readded a test

--- parallelRepublisher_test.go
+++ parallelRepublisher_test.go
@@ -1,16 +1,22 @@
 package main
 
-// func TestSingle_Ok(t *testing.T) {
-// 	mockedRepublisher := new(mockedRepublisher)
+import (
+	"testing"
 
-// 	mockedRepublisher.On("RepublishUUID", "19cf2763-90b1-40db-90e7-e813425ebe81", scopeBoth, "prefix1").Return()
+	"github.com/stretchr/testify/mock"
+)
 
-// 	pRepublisher := newNotifyingParallelRepublisher(mockedRepublisher, 1, 1)
+func TestSingle_Ok(t *testing.T) {
+	mockedUUIDRepublisher := new(mockUUIDRepublisher)
+	okMsg := "sent for publish uuid=19cf2763-90b1-40db-90e7-e813425ebe81 tid=prefix1 collection=collection1 originSystemId=originSystemId1 size=1024 notifierApp=cms-notifier"
+	mockedUUIDRepublisher.On("Republish", "19cf2763-90b1-40db-90e7-e813425ebe81", "prefix1", scopeBoth).Return([]string{okMsg}, []error{})
 
-// 	pRepublisher.Republish([]string{"19cf2763-90b1-40db-90e7-e813425ebe81"}, scopeBoth, "prefix1")
+	pRepublisher := newNotifyingParallelRepublisher(mockedUUIDRepublisher, 1)
 
-// 	mock.AssertExpectationsForObjects(t, mockedRepublisher)
-// }
+	pRepublisher.Republish([]string{"19cf2763-90b1-40db-90e7-e813425ebe81"}, scopeBoth, "prefix1")
+
+	mock.AssertExpectationsForObjects(t, mockedUUIDRepublisher)
+}
 
 // func TestParallel_Ok(t *testing.T) {
 // 	mockedRepublisher := new(mockedRepublisher)
@@ -30,10 +36,11 @@ package main
 // 	mock.AssertExpectationsForObjects(t, mockedRepublisher)
 // }
 
-// type mockedRepublisher struct {
-// 	mock.Mock
-// }
+type mockUUIDRepublisher struct {
+	mock.Mock
+}
 
-// func (m *mockedRepublisher) RepublishUUID(uuid string, republishScope string, tidPrefix string) {
-// 	_ = m.Called(uuid, republishScope, tidPrefix)
-// }
+func (m *mockUUIDRepublisher) Republish(uuid, tidPrefix string, republishScope string) (msgs []string, errs []error) {
+	args := m.Called(uuid, tidPrefix, republishScope)
+	return args.Get(0).([]string), args.Get(1).([]error)
+}
