e2bd9e63be19e9545005629b018bd4b14a70f87b
ekaneva <elina.kaneva@ft.com>
2020-10-16 14:03:59 +0300

    Simplify republishing logic

--- pkg/republisher/uuid_republisher.go
+++ pkg/republisher/uuid_republisher.go
@@ -31,9 +31,10 @@ func NewNotifyingUUIDRepublisher(uuidCollectionRepublisher UUIDCollectionRepubli
 func (r *NotifyingUUIDRepublisher) Republish(uuid, tidPrefix string, republishScope string) (msgs []*OKMsg, errs []error) {
 	isFoundInAnyCollection := false
 	isScopedInAnyCollection := false
-	foundInUC := false
+	priorityCollection := r.collections["universal-content"]
+	isFoundInPriorityCollection := false
 
-	auxRepublish := func(collection CollectionMetadata) []*OKMsg {
+	republishFrom := func(collection CollectionMetadata) []*OKMsg {
 		tid := tidPrefix + transactionidutils.NewTransactionID()
 		isScopedInAnyCollection = true
 		msg, isFound, err := r.ucRepublisher.RepublishUUIDFromCollection(uuid, tid, collection)
@@ -49,28 +50,26 @@ func (r *NotifyingUUIDRepublisher) Republish(uuid, tidPrefix string, republishSc
 		return msgs
 	}
 
-	// If the scope matches spark's one - independently the uuid's type, there is no way to know that at this stage-
-	// 1 - it tries to republish it first in universal-content collection (spark) in case it succeed we're done
-	// 2 - in case the scope is ScopeBoth (content & metadata) and it has been republished in spark then it proceed to republish in metadata
-	// 3 - In case none of the above match then tries the old way, iterating all the collections, and whenever there is a match the uuid is republished. (could be more than one time)
-	// this is the current behaviour.
-	if republishScope == r.collections["universal-content"].scope || republishScope == ScopeBoth {
-		msgs = auxRepublish(r.collections["universal-content"])
-		foundInUC = isFoundInAnyCollection
-	}
-
-	if foundInUC && republishScope == ScopeBoth {
-		for _, collection := range r.collections {
-			if collection.scope == ScopeMetadata {
-				msgs = auxRepublish(collection)
+	if republishScope == ScopeBoth || republishScope == ScopeContent {
+		// try priority content collection first
+		msgs = republishFrom(priorityCollection)
+		isFoundInPriorityCollection = isFoundInAnyCollection
+		// if not found in priority, try all other content
+		if !isFoundInPriorityCollection {
+			for _, collection := range r.collections {
+				if collection.scope == ScopeContent {
+					msgs = republishFrom(collection)
+				}
 			}
 		}
-	} else if !isFoundInAnyCollection {
+	}
+
+	// republish metadata when scope requires it
+	if republishScope == ScopeBoth || republishScope == ScopeMetadata {
 		for _, collection := range r.collections {
-			if republishScope != ScopeBoth && republishScope != collection.scope {
-				continue
+			if collection.scope == ScopeMetadata {
+				msgs = republishFrom(collection)
 			}
-			msgs = auxRepublish(collection)
 		}
 	}
 
